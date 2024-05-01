3bc2e7a0e0419f237129e6f4e363c3584953b3ff
Sofia <sofia.recreio2@gmail.com>
2024-05-01 16:51:04 -0300

    Innitial commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { env } from './env'
+import { app } from './app'
+
+app.listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+}).then(() => {
+    console.log('HTTP Server Running!')
+})
